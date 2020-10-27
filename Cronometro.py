7de521f3db97e1524de6d1a2c14d6f66b03d440b
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2020-10-27 08:36:07 -0300

    Update Cronometro.py

--- Cronometro.py
+++ Cronometro.py
@@ -1,29 +1,47 @@
+import datetime
 import time
 from math import trunc
 
-print('''-=-=-=Cronometro Pyton-=-=-=-=-''')
+exit = False
+
+print('-=-=-=Cronometro Pyton-=-=-=-=-')
 while not exit:
-  print('''Seleciono modo de uso:
-  Cronometro [1]
-  Data [2]
-  Medidor de velocidade [3]''')
-  
-  cod = int(input('->_ '))
-  
-  if cod != 1 and cod != 2 and cod != 3:
-    print('Número inválido! Tente novamente')
+    print('''Seleciono modo de uso:
+
+Cronometro [1]
+Data [2]
+Fechar o programa [3]''')
+
     cod = int(input('->_ '))
-  
-  elif cod == 1:
-    print('Para inicialzar o cronometro digite ´OK`')
-    start = str(input('Digite sua confirmação: ')).strip().upper()
-    if start == 'OK':
-      timer_str = time.time()
-      end = int(input('Para parar o cronometro digite 0 _-> '))
-      timer_end = time.time()
-      timer_calc = (timer_end - timer_str)
-      current = trunc(timer_calc)
-      if current > 60:
-        calc = (current/60)
-        minutos = calc
-        segundos = (current//60)
+
+    if cod != 1 and cod != 2 and cod != 3:
+        print('Número inválido! Tente novamente')
+        cod = int(input('->_ '))
+
+    elif cod == 1:
+        print('Para inicialzar o cronometro digite ´OK`')
+        start = str(input('Digite sua confirmação: ')).strip().upper()
+        if start == 'OK':
+            timer_str = time.time()
+            print('O cronometro está em execução...')
+            end = input('Para parar o cronometro pressione qualquer tecla')
+            timer_end = time.time()
+            timer_calc = (timer_end - timer_str)
+            current = trunc(timer_calc)
+            segundos = current
+            minutos = 0
+            if current > 60:
+                calc = (current / 60)
+                minutos = calc
+                segundos = (current % 60)
+        print('Tempo decorrido: {}min  {}sec'.format(minutos, current))
+        print('''
+        ''')
+    elif cod == 2:
+        print('Data atual: {}'.format(datetime.date.today()))
+        print('''
+        ''')
+    elif cod == 3:
+        exit = True
+print('processo finalizado')
+time.sleep(3)
